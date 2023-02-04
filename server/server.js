2d8f31be0cc3487a79e5775d51c7e3b31a5796b6
AmyLipscomb <amy.a.lipscomb@gmail.com>
2023-02-04 14:03:12 -0500

    added server folder and server.js

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 7000;
+
+
+// Serves static files in the entire client's dist folder
+app.use(express.static('../client/dist'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Requires HTML route
+require('./routes/htmlRoutes')(app);
+
+// Makes sure the server is alive
+app.listen(PORT, () => console.log(`Now listening on port http://localhost:${PORT}`));
