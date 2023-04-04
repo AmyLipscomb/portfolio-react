7cda57d06ae1dd0746e63c52da334cca7739f2d8
AmyLipscomb <amy.a.lipscomb@gmail.com>
2023-04-03 20:00:28 -0400

    updated background color and text for 'about me' page, updated resume

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -35,7 +35,8 @@ const Header = () => {
                         <a href="https://github.com/AmyLipscomb?tab=repositories">Github</a>
                     </li> */}
                     <li>
-                        <a href="assets/images/portfolio_resume.pdf" target="_blank">Resume</a>
+                        <a href="assets/images/amylipscomb_webdeveloper_resume_v1.pdf"
+                         target="_blank">Resume</a>
                         {/* Replace with actual resume (will need to save resume in public folder) */}
                     </li>
                 </ul>
